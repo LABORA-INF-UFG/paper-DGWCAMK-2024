679e6248872162ac6875b4402c9ea614ef732b31
DanteCampos <dcamposfsa@gmail.com>
2024-01-11 15:43:59 +0000

    Separate plots

--- simulation/plotter.py
+++ simulation/plotter.py
@@ -88,9 +88,9 @@ class Plotter:
             if bs_names is not None and bs.name not in bs_names:
                 continue
             if normalize:
-                metric = bs.hist_n_allocated_RBGs
-            else:
                 metric = np.array(bs.hist_n_allocated_RBGs)/len(bs.rbgs)
+            else:
+                metric = bs.hist_n_allocated_RBGs
             downsampled = [np.mean(bs.hist_n_allocated_RBGs[i:i+density]) for i in range(0, len(bs.hist_n_allocated_RBGs), density)]
             x_ticks = np.arange(0, self.sim.step, density)
             plt.plot(x_ticks, downsampled, label="{}".format(bs.name))
@@ -115,9 +115,9 @@ class Plotter:
                 if slice_types is not None and slice.type not in slice_types:
                     continue
                 if normalize:
-                    metric = slice.hist_n_allocated_RBGs
-                else:
                     metric = np.array(slice.hist_n_allocated_RBGs)/len(bs.rbgs)
+                else:
+                    metric = slice.hist_n_allocated_RBGs
                 downsampled = [np.mean(metric[i:i+density]) for i in range(0, len(slice.hist_n_allocated_RBGs), density)]
                 x_ticks = np.arange(0, self.sim.step, density)
                 if slice_types is not None and len(slice_types) == 1:
@@ -137,6 +137,8 @@ class Plotter:
         plt.close('all')
     
     def plot_slice_fifth_perc_thr(self, window: int, density: int = 1, bs_names: List[str] = None, slice_types: List[str] = None, value:str = "average"):
+        if value not in ["average", "worst"]:
+            raise Exception("Value must be average or worst")
         plot_requirement = False
         if slice_types is not None and len(slice_types) == 1:
             plt.figure("Fifth-percentile throughput of {}".format(slice_types[0]))
@@ -160,25 +162,33 @@ class Plotter:
                         real_window = min(window, step+1)
                         metric = np.percentile(user.hist_allocated_throughput[step-real_window+1:step+1], 5) /1e6 # bits/s -> Mbps
                         ue_fifth_per_thr[user_id].append(metric)
-                for step in range(len(slice.hist_n_allocated_RBGs)):
-                    slice_fifth_per_thr.append(np.mean([ue_fifth_per_thr[user_id][step] for user_id in ue_fifth_per_thr]))
+                if value == "average":
+                    for step in range(len(slice.hist_n_allocated_RBGs)):
+                        slice_fifth_per_thr.append(np.mean([ue_fifth_per_thr[user_id][step] for user_id in ue_fifth_per_thr]))
+                elif value == "worst":
+                    slice_fifth_per_thr = np.min([ue_fifth_per_thr[user_id] for user_id in ue_fifth_per_thr], axis=0)
                 downsampled = [np.mean(slice_fifth_per_thr[i:i+density]) for i in range(0, len(slice_fifth_per_thr), density)]
                 x_ticks = np.arange(0, self.sim.step, density)
                 if slice_types is not None and len(slice_types) == 1:
                     plt.plot(x_ticks, downsampled, label="{}".format(bs.name))
-                    
                 else:
                     plt.plot(x_ticks, downsampled, label="{}-{}".format(slice.type, bs.name))
         plt.xlabel("Time (ms)")
         plt.ylabel("Throughput (Mbps)")
         plt.legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=5)
+        filename = "fifth_perc_thr.pdf"
+        if value == "worst":
+            filename = "worst_" + filename
         if slice_types is None:
-            plt.savefig(self.path + "slice_fifth_perc_thr.pdf")
+            filename = "slice_" + filename
         else:
-            plt.savefig(self.path + "_".join(slice_types) +"_fifth_perc_thr.pdf")
+            filename = "_".join(slice_types) + "_" + filename
+        plt.savefig(self.path + filename)
         plt.close('all')
     
     def plot_slice_long_term_thr(self, window: int, density: int = 1, bs_names: List[str] = None, slice_types: List[str] = None, value:str = "average"):
+        if value not in ["average", "worst"]:
+            raise Exception("Value must be average or worst")
         plot_requirement = False
         if slice_types is not None and len(slice_types) == 1:
             plt.figure("Long term throughput of {}".format(slice_types[0]))
@@ -202,8 +212,11 @@ class Plotter:
                         real_window = min(window, step+1)
                         metric = np.mean(user.hist_allocated_throughput[step-real_window+1:step+1]) /1e6 # bits/s -> Mbps
                         ue_long_term_thr[user_id].append(metric)
-                for step in range(len(slice.hist_n_allocated_RBGs)):
-                    slice_long_term_thr.append(np.mean([ue_long_term_thr[user_id][step] for user_id in ue_long_term_thr]))
+                if value == "average":
+                    for step in range(len(slice.hist_n_allocated_RBGs)):
+                        slice_long_term_thr.append(np.mean([ue_long_term_thr[user_id][step] for user_id in ue_long_term_thr]))
+                elif value == "worst":
+                    slice_long_term_thr = np.min([ue_long_term_thr[user_id] for user_id in ue_long_term_thr], axis=0)
                 downsampled = [np.mean(slice_long_term_thr[i:i+density]) for i in range(0, len(slice_long_term_thr), density)]
                 x_ticks = np.arange(0, self.sim.step, density)
                 if slice_types is not None and len(slice_types) == 1:
@@ -213,13 +226,19 @@ class Plotter:
         plt.xlabel("Time (ms)")
         plt.ylabel("Throughput (Mbps)")
         plt.legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=5)
+        filename = "long_term_thr.pdf"
+        if value == "worst":
+            filename = "worst_" + filename
         if slice_types is None:
-            plt.savefig(self.path + "slice_long_term_thr.pdf")
+            filename = "slice_" + filename
         else:
-            plt.savefig(self.path + "_".join(slice_types) +"_long_term_thr.pdf.pdf")
+            filename = "_".join(slice_types) + "_" + filename
+        plt.savefig(self.path + filename)
         plt.close('all')
     
     def plot_slice_throughput(self, density: int = 1, bs_names: List[str] = None, slice_types: List[str] = None, value:str = "average"):
+        if value not in ["average", "worst"]:
+            raise Exception("Value must be average or worst")
         plot_requirement = False
         if slice_types is not None and len(slice_types) == 1:
             plt.figure("Served throughput of {}".format(slice_types[0]))
@@ -235,7 +254,10 @@ class Plotter:
                 if plot_requirement:
                     plt.axhline(y=slice.requirements["throughput"]/1e6, color='r', linestyle='--', label="requirement")
                     plot_requirement = False
-                metric = np.array(slice.hist_allocated_throughput)/1e6
+                if value == "average":
+                    metric = np.array(slice.hist_allocated_throughput)/1e6
+                elif value == "worst":
+                    metric = np.min([ue.hist_allocated_throughput for ue in slice.users.values()], axis=0)/1e6
                 downsampled = [np.mean(metric[i:i+density]) for i in range(0, len(metric), density)]
                 x_ticks = np.arange(0, self.sim.step, density)
                 if slice_types is not None and len(slice_types) == 1:
@@ -245,14 +267,19 @@ class Plotter:
         plt.xlabel("Time (ms)")
         plt.ylabel("Throughput (Mbps)")
         plt.legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=5)
-        plt.savefig(self.path + "slice_served_thr.pdf")
+        filename = "served_thr.pdf"
+        if value == "worst":
+            filename = "worst_" + filename
         if slice_types is None:
-            plt.savefig(self.path + "slice_served_thr.pdf")
+            filename = "slice_" + filename
         else:
-            plt.savefig(self.path + "_".join(slice_types) +"_served_thr.pdf.pdf")
+            filename = "_".join(slice_types) + "_" + filename
+        plt.savefig(self.path + filename)
         plt.close('all')
     
     def plot_slice_avg_buff_lat(self, density: int = 1, bs_names: List[str] = None, slice_types: List[str] = None, value:str = "average"):
+        if value not in ["average", "worst"]:
+            raise Exception("Value must be average or worst")
         plot_requirement = False
         if slice_types is not None and len(slice_types) == 1:
             plt.figure("Average Buffer Latency of {}".format(slice_types[0]))
@@ -268,10 +295,17 @@ class Plotter:
                 if plot_requirement:
                     plt.axhline(y= slice.requirements["latency"]*self.sim.TTI*1e3, color='r', linestyle='--', label="requirement")
                     plot_requirement = False
-                slice_avg_buff_lat = []
-                for step in range(self.sim.step):
-                    metric = np.mean([user.hist_avg_buff_lat[step] for user in slice.users.values()]) * 1e3
-                    slice_avg_buff_lat.append(metric)
+                if value == "average":
+                    slice_avg_buff_lat = np.mean([user.hist_avg_buff_lat for user in slice.users.values()], axis=0) * 1e3
+                elif value == "worst":
+                    slice_avg_buff_lat = np.max([user.hist_avg_buff_lat for user in slice.users.values()], axis=0) * 1e3
+                # slice_avg_buff_lat = []
+                # for step in range(self.sim.step):
+                #     if value == "average":
+                #         metric = np.mean([user.hist_avg_buff_lat[step] for user in slice.users.values()]) * 1e3
+                #     elif value == "worst":
+                #         metric = np.min([user.hist_avg_buff_lat[step] for user in slice.users.values()]) * 1e3
+                #     slice_avg_buff_lat.append(metric)
                 downsampled = [np.mean(slice_avg_buff_lat[i:i+density]) for i in range(0, len(slice_avg_buff_lat), density)]
                 x_ticks = np.arange(0, self.sim.step, density)
                 if slice_types is not None and len(slice_types) == 1:
@@ -281,13 +315,19 @@ class Plotter:
         plt.xlabel("Time (ms)")
         plt.ylabel("Latency (ms)")
         plt.legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=5)
+        filename = "avg_buff_lat.pdf"
+        if value == "worst":
+            filename = "worst_" + filename
         if slice_types is None:
-            plt.savefig(self.path + "slice_avg_buff_lat.pdf")
+            filename = "slice_" + filename
         else:
-            plt.savefig(self.path + "_".join(slice_types) +"_avg_buff_lat.pdf.pdf")
+            filename = "_".join(slice_types) + "_" + filename
+        plt.savefig(self.path + filename)
         plt.close('all')
     
     def plot_slice_pkt_loss_rate(self, window: int, density: int = 1, bs_names: List[str] = None, slice_types: List[str] = None, value:str = "average"):
+        if value not in ["average", "worst"]:
+            raise Exception("Value must be average or worst")
         plot_requirement = False
         if slice_types is not None and len(slice_types) == 1:
             plt.figure("Packet Loss Rate of {} for {}TTIs window".format(slice_types[0], window))
@@ -304,15 +344,23 @@ class Plotter:
                     plt.axhline(y= slice.requirements["pkt_loss"]*100, color='r', linestyle='--', label="requirement")
                     plot_requirement = False
                 slice_pkt_loss = []
-                for step in range(self.sim.step):
-                    real_window = min(window, step+1)
-                    metric = np.mean(
-                        [
-                        sum(user.hist_dropp_pkt_bits[-window:])/(sum(user.hist_arriv_pkt_bits[-window:]) + user.hist_buff_pkt_bits[-window])
-                        for user in slice.users.values()
-                        ]
-                    ) * 100
-                    slice_pkt_loss.append(metric)
+                
+                ue_pkt_loss: Dict[int, List[float]] = dict()
+                for user_id, user in slice.users.items():
+                    ue_pkt_loss[user_id] = []
+                    for step in range(len(user.hist_dropp_pkt_bits)):
+                        real_window = min(window, step+1)
+                        numerator = int(sum(user.hist_dropp_pkt_bits[step-real_window+1:step+1]))
+                        denominator = int(sum(user.hist_arriv_pkt_bits[step-real_window+1:step+1]) + user.hist_buff_pkt_bits[step-real_window+1])
+                        if denominator == 0:
+                            ue_pkt_loss[user_id].append(0)
+                        else:
+                            ue_pkt_loss[user_id].append(float(numerator/denominator))
+                    ue_pkt_loss[user_id] = np.array(ue_pkt_loss[user_id])
+                if value == "average":
+                    slice_pkt_loss = np.mean([ue_pkt_loss[user_id] for user_id in slice.users.keys()], axis=0) * 100
+                elif value == "worst":
+                    slice_pkt_loss = np.max([ue_pkt_loss[user_id] for user_id in slice.users.keys()], axis=0) * 100
                 downsampled = [np.mean(slice_pkt_loss[i:i+density]) for i in range(0, len(slice_pkt_loss), density)]
                 x_ticks = np.arange(0, self.sim.step, density)
                 if slice_types is not None and len(slice_types) == 1:
@@ -322,8 +370,12 @@ class Plotter:
         plt.xlabel("Time (ms)")
         plt.ylabel("Rate (%)")	
         plt.legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=5)
+        filename = "pkt_loss.pdf"
+        if value == "worst":
+            filename = "worst_" + filename
         if slice_types is None:
-            plt.savefig(self.path + "slice_pkt_loss.pdf")
+            filename = "slice_" + filename
         else:
-            plt.savefig(self.path + "_".join(slice_types) +"_pkt_loss.pdf.pdf")
+            filename = "_".join(slice_types) + "_" + filename
+        plt.savefig(self.path + filename)
         plt.close('all')
